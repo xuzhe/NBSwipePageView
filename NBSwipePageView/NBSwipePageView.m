89bb1f807ae8341c0946451d8c8d4e6b10a2c406
Xu Zhe <xu.zhe@rakutec.com>
2015-07-14 00:22:08 +0900

    update copyright info

--- NBSwipePageView/NBSwipePageView.m
+++ NBSwipePageView/NBSwipePageView.m
@@ -3,7 +3,7 @@
 //  NBSwipePageView
 //
 //  Created by 徐 哲 on 4/25/12.
-//  Copyright (c) 2012 ラクラクテクノロジーズ株式会社 Rakuraku Technologies, Inc. All rights reserved.
+//  Copyright (c) 2012 ラクラクテクノロジーズ株式会社 XUZHE.COM. All rights reserved.
 //
 
 #import "NBSwipePageView.h"
